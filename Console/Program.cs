e4ec46354203d46b0d3dad7eb67a4ceb6305d7bd
vital <fatexik@gmail.com>
2018-12-02 13:38:10 +0300

    Test console application

---
+++ Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TestGenerator;
+
+namespace Console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int readingTaskCount = Convert.ToInt32(args[0]);
+            int writeTaskCount = Convert.ToInt32(args[1]);
+            string outpDirectory = args[2];
+            List<string> paths = new List<string>();
+            for (int i = 3; i < args.Length; i++)
+            {
+                paths.Add(args[i]);
+            }
+            Reader reader = new Reader();
+            Writer writer = new Writer(outpDirectory);
+            TestGeneratorClass.TestGeneratorClass testGeneratorClass = new TestGeneratorClass.TestGeneratorClass(outpDirectory);
+
+            testGeneratorClass.generate(readingTaskCount, writeTaskCount, paths).Wait();
+            
+            System.Console.WriteLine("Done !");
+            System.Console.ReadLine();
+        }        
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using TestGenerator;
+using TestGenerator = TestGenerator.TestGenerator;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int readingTaskCount = Convert.ToInt32(args[0]);
+            int writeTaskCount = Convert.ToInt32(args[1]);
+            string outpDirectory = args[2];
+            List<string> paths = new List<string>();
+            for (int i = 3; i < args.Length; i++)
+            {
+                paths.Add(args[i]);
+            }
+            TestGenerator.TestGenerator testGenerator = new TestGenerator(outpDirectory);
+            testGenerator.
+        }
+    }
+}
