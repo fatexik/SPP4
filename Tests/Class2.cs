d7c422366660c01b2238298989753a1990384578
vital <fatexik@gmail.com>
2018-12-03 23:28:31 +0300

    Tests, refactoring code

---
+++ Tests/Class2.cs
@@ -0,0 +1,27 @@
+namespace Tests
+{
+    public class Class2
+    {
+        public int count;
+
+        public void FirstMethod()
+        {
+            // smth
+        }
+
+        public void SecondMethod()
+        {
+            // smth
+        }
+
+        public void ThirdMethod()
+        {
+            // smth
+        }
+
+        private void FourthMethod()
+        {
+            // smth
+        }
+    }
+}
